21f9fce0db6e349c1723fcc6b2463ee282d19d93
minhduc22799 <minhduc22799@gmail.com>
2022-10-05 10:17:06 +0700

    commit new

---
+++ IntroductionToJava/day-of-month/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter month");
+        int month = scanner.nextInt();
+
+        switch (month){
+            case 2:
+                System.out.println("Thang"+month+" co 28 hoac 29 ngay");
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("Thang "+month+"  co 31 ngay");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("Thang "+month+"  co 30 ngay");
+                break;
+            default:
+                System.out.println("khon co thang " + month);
+        }
+    }
+}
