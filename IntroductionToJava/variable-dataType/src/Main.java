e352be0a61cd892d175ddc0b29740d3ed4439330
minhduc22799 <minhduc22799@gmail.com>
2022-10-05 09:49:30 +0700

    commit new

---
+++ IntroductionToJava/variable-dataType/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+       int i = 5;
+       float f = 0.5f;
+       double d = 20.4;
+       boolean b = true;
+       char c = 'd';
+       String s = "Dinh Duc";
+
+
+        System.out.println("i = "+i);
+        System.out.println("f = "+f);
+        System.out.println("d = "+d);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("s = "+s);
+    }
+}
