71595f538822915fade2ef74e9df319890eef7ed
minhduc22799 <minhduc22799@gmail.com>
2022-10-14 10:36:07 +0700

    commit new

---
+++ KeThua/src/Point/Main.java
@@ -0,0 +1,11 @@
+package Point;
+
+public class Main {
+    public static void main(String[] args) {
+        Point2D point2D = new  Point2D();
+        System.out.println(point2D);
+        Point3D point3D = new Point3D();
+        System.out.println(point3D);
+
+    }
+}
