a418c1cf061410be2c5fdd9f0e7d9763079c1f5a
minhduc22799 <minhduc22799@gmail.com>
2022-10-28 09:53:28 +0700

    commit new

---
+++ Thread/bai_tap/OddThread.java
@@ -0,0 +1,15 @@
+package bai_tap;
+
+public class OddThread extends Thread{
+    public  void run(){
+        for (int i =0; i<=10; i++){
+            if (i%2 !=0) {
+                System.out.println(i + "\t");
+            }
+        }try {
+            Thread.sleep(10);
+        }catch (InterruptedException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ Thread/bai_tap/ThreadMain.java
@@ -0,0 +1,18 @@
+package bai_tap;
+
+public class ThreadMain {
+    public static void main(String[] args) {
+        OddThread oddThread = new OddThread();
+        EvenThread evenThread = new EvenThread();
+
+        Thread thread = new Thread(oddThread);
+        Thread thread1 = new Thread(evenThread);
+        thread.start();
+        try {
+            thread.join(100);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        thread1.start();
+    }
+}
